9508a5b20a87df5f873d29fc37bf0057665fba7b
Stanley Tsonev <stanleytsonev@hotmail.com>
2022-12-04 17:23:40 -0500

    more jwt auth

--- pages/api/employees.ts
+++ pages/api/employees.ts
@@ -1,14 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../lib/prisma';
-
-type Data = {
-  name: string;
-};
+import { validateToken } from '../../lib/serverfunctions';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const employees = await prisma.employee.findMany();
-  res.status(200).json(employees);
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    res.status(401).json({ error: 'unauthorized' });
+  } else {
+    const isValid = await validateToken(token);
+    if (isValid) {
+      const employees = await prisma.employee.findMany();
+      res.status(200).json(employees);
+    } else {
+      res.status(401).json({ error: 'unauthorized' });
+    }
+  }
 }
